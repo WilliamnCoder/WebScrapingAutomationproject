50dc438a2ff22a8e536bc5f12c579c4d7c026ed9
William <121023616+Userwr20@users.noreply.github.com>
2024-12-07 14:58:17 +0100

    added 'requests' and changed places with bs4 import, along with comments

--- WebScrapingAutomation.py
+++ WebScrapingAutomation.py
@@ -1,2 +1,2 @@
-# Here the script will be made for web scraping
-from bs4 import BeautifulSoup
+import requests # imported to fetch web content (also possible with 'urllib' but opted for this due to its simplicity)
+from bs4 import BeautifulSoup #importing BeautifulSoup for parsing the HTML page(s)
