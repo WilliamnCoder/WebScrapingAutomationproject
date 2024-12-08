a27487ff8e44c1e6a09bf0798d9ac23415d974e0
William <121023616+Userwr20@users.noreply.github.com>
2024-12-08 21:58:14 +0100

    added variables url, respone and soup

--- WebScrapingAutomation.py
+++ WebScrapingAutomation.py
@@ -1,2 +1,7 @@
 import requests # imported to fetch web content (also possible with 'urllib' but opted for this due to its simplicity)
 from bs4 import BeautifulSoup #importing BeautifulSoup for parsing the HTML page(s)
+
+url = "https://books.toscrape.com/catalogue/sharp-objects_997/index.html"
+response = requests.get(url)
+soup = BeautifulSoup(response.text, 'html.parser')
+
