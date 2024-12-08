f588894dd03dc26e10eabf86948d10462d1d330e
William <121023616+Userwr20@users.noreply.github.com>
2024-12-08 22:42:12 +0100

    Added response.encoding ('utf-8') to be able to decode symbols according to uft-8

--- WebScrapingAutomation.py
+++ WebScrapingAutomation.py
@@ -3,11 +3,13 @@ from bs4 import BeautifulSoup #importing BeautifulSoup for parsing the HTML page
 
 url = "https://books.toscrape.com/catalogue/sharp-objects_997/index.html"
 response = requests.get(url) # "response" stores the servers response. The other part of the code is a GET request to fetch the HTML via the url defined above
+response.encoding = 'utf-8'
 soup = BeautifulSoup(response.text, 'html.parser')
 
 title = soup.find('h1').text.strip()  # this line finds and then extracts the title of the book by finding the first <h1> HTML tag in the url
-price = soup.find('p',{'class': 'price_color'}).text.strip()
+price = soup.find('p',{'class': 'price_color'}).text.strip() ## TO DO: remove unwanted symbols before output
 
 print(f"Title: {title}")
 print(f"Price: {price}")
+
 
